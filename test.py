4562f6bfb2dc8d5154949ac8b75eb0f8558a6905
Chenxia Han <cxhan@cse.cuhk.edu.hk>
2020-12-13 16:25:35 +0800

    fix test

--- test.py
+++ test.py
@@ -15,6 +15,12 @@ def parse_args():
     parser.add_argument("--checkpoint-path", required=True)
     parser.add_argument("--save-path", required=True)
 
+    parser.add_argument("--knobs-set", type=str, default="mini_knobs")
+    parser.add_argument("--max-episode-length", type=int, default=50)
+    parser.add_argument("--minimal-score", type=float, default=-10.0)
+    parser.add_argument("--num_metrics", type=int, default=74)
+    parser.add_argument("--workload-type", type=str, default="read")
+
     args = parser.parse_args()
 
     return args
@@ -34,20 +40,38 @@ if __name__ == "__main__":
         "password": "123456",
         "database": "sbtest",
         "memory": 4 * 1024 * 1024 * 1024, # Bytes = 4GB
-        "knobs_set": "mini_knobs",
+        "knobs_set": args.knobs_set,
     }
     mysql_handle = MySQLConnector(mysql_config)
 
     # prepare sysbench
-    sysbench_handle = SysBenchSimulator()
+    sysbench_handle = SysBenchSimulator(workload=args.workload_type)
 
     config = {
-        "num_metrics": 74,
-        "db_handle": mysql_handle,
-        "simulator_handle": sysbench_handle,
+        "env": MySQLEnv,  # "CarRacing-v0",
+        "env_config": {
+            "num_metrics": args.num_metrics,
+            "max_episode_length": args.max_episode_length,
+            "minimal_score": args.minimal_score,
+            "db_handle": mysql_handle,
+            "simulator_handle": sysbench_handle,
+        },
+        "num_gpus": 0,
+        "num_workers": 0,
+        "framework": "torch",
+        # "train_batch_size": 16,
+        # "tau": 1e-5,
+        # "actor_lr": 1e-5,
+        # "critic_lr": 1e-5,
+        # "gamma": 0.9,
+        # "buffer_size": 100000,
+        # "learning_starts": 0,
+        # "timesteps_per_iteration": args.steps_per_iter,
+        # "evaluation_interval": 0,
+        # "evaluation_num_episodes": 0,
     }
-    env = MySQLEnv(config)
-    agent = ddpg.DDPGTrainer(config=config, env=MySQLEnv)
+
+    agent = ddpg.DDPGTrainer(config=config)
     agent.restore(args.checkpoint_path)
 
     done = False
